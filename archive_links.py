2e7a2c232508ba0a3a3ae3b2507203a6e86f52e1
jonasengelmann <jonenone+github@gmail.com>
2019-03-25 21:36:13 +0100

    fixed issues + made arguments mutually exclusive

--- archive_links.py
+++ archive_links.py
@@ -9,7 +9,8 @@ from urlextract import URLExtract
 
 def clean_url(url):
     url = url.strip(r'()[]{}')
-    if not url.lower().startswith(('http','ftp://')):
+    url = url.replace("ftp://", "http://")
+    if not url.lower().startswith('http'):
         url = f'http://{url}'
     return url
 
@@ -55,7 +56,7 @@ def archive_url(url, force_new_archive=False):
         # check if url already archived
         archived_url = fetch_archived_url_from_waybackmachine(url)
 
-    if not archive_url:
+    if not archived_url:
         archived_url = push_url_to_waybackmachine(url)
 
     if archived_url:
@@ -66,41 +67,38 @@ def archive_url(url, force_new_archive=False):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--url', help='URL you want to archive.', type=str)
-    parser.add_argument('-t','--textfile',
-                        help='''Path to a textfile containing urls. The script 
-                                will make a copy of the textfile and replace all
-                                urls with their archived version. The textfile 
-                                can also contain non url content.''', type=str)
-    parser.add_argument('-f', '--force',
-                        help = '''When an an url is already archived, we can
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-u', '--url', help='URL you want to archive.', type=str)
+    group.add_argument('-f', '--file', help='''Path to a textfile containing urls. The
+                                  script will make a copy of the textfile and
+                                  replace all urls with their archived version.
+                                  The textfile can also contain non url
+                                  content.''', type=str)
+    parser.add_argument('--force',
+                        help='''When an an url is already archived, we can
                                   use this parameter to assure a new archived
                                   version is created.''',
-                        action = 'store_true')
+                        action='store_true')
 
 
     args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG, format='%(levelname)-8s%(message)s')
 
-    if not args.url and not args.textfile:
-        logging.error('Please specify an url or a textfile with containing urls!')
-        sys.exit(1)
-    
-    logging.info('Archiving links...')
+    logging.info('Archiving urls...')
 
     if args.url:
         url = clean_url(args.url)
         archive_url(url, args.force)
-        
-    if args.textfile:
-        with open(args.textfile, 'r') as in_file:
+
+    if args.file:
+        with open(args.file, 'r') as in_file:
             data = in_file.read()
         
-        with open(args.textfile + "_urls_archived", 'w') as out_file:
+        with open(args.file + "_urls_archived", 'w') as out_file:
             for url in find_urls_in_text(data):
                 new_url = archive_url(clean_url(url), args.force)
                 if new_url:
                     data = data.replace(url, new_url)
             out_file.write(data)
-    
+
     logging.info("Done!")
